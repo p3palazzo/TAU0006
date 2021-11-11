ddbb718bb68316d8d8ffb5dea73038b67451c4bc
Automated build <runner@github.com>
2021-11-11 22:54:55 +0000

    Workflow com pull --rebase

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "Apresentação e acolhimento",
+        "excerpt":"Boas-vindas Esta é a Turma C da disciplina História da Arquitetura e da Arte II. Vamos acompanhar vocês num percurso que vai do século XV ao XVIII. Começamos falando um pouco sobre como a disciplina vai ser desenvolvida este semestre — se você está com dúvidas sobre o funcionamento das...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/00-intro/",
+        "teaser": null
+      },{
+        "title": "Arquitetura do humanismo",
+        "excerpt":"Introdução à arquitetura da Idade Moderna O nosso percurso começa em 1420, mais ou menos. Meio século para mais ou para menos, mas isso não importa. Exatamente em 1420, termina a reconstrução do palácio imperial em Pequim, conhecido no Ocidente como a “Cidade proibida” (fig. 1). No mesmo ano, começa a...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/01-humanismo/",
+        "teaser": null
+      },{
+        "title": "Rotas comerciais e paradigmas urbanísticos",
+        "excerpt":"First terrestrial globe - Martin Behaim 1492 by Drones Imaging on Sketchfab Parcelamento do solo e paisagem urbana Os elementos e objetos da arquitetura têm uma ordem de precedência específica. A tipologia edilícia determina os padrões de ocupação do espaço urbano e rural: isso porque os tipos de casas usados...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/02-rotas/",
+        "teaser": null
+      },{
+        "title": "Renascimento e historiografia",
+        "excerpt":"Nesta sequência, vamos abordar a arquitetura do século XV na Europa e na Ásia central sob os pontos de vista da construção e da espacialidade. O desenvolvimento de sistemas e processos construtivos e a transformação nas configurações e relações espaciais são duas das principais questões que pertencem ao domínio da...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/03-renascimento/",
+        "teaser": null
+      },{
+        "title": "Cidades ideais e cidades possíveis",
+        "excerpt":"Neste tópico nós vamos tratar do pensamento e da prática de produção das cidades na Europa e na Ásia dos séculos XV e XVI. A primeira parte aborda o que se convencionou chamar de cidades ideais na teoria da arquitetura na Itália e na China. A segunda parte observa a...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/04-cidades-ideais/",
+        "teaser": null
+      },{
+        "title": "Teoria do classicismo",
+        "excerpt":"Teoria das ordens Ordens por divisões tripartites sucessivas (Vitrúvio e Alberti). Demonstrar com base e entablamento. Ordens com proporções numéricas (Vinhola e Palladio) Teoria das molduras Na tradição arquitetônica europeia, as ordens clássicas são o instrumento por excelência para realizar articulações de superfícies e espaços. A aparência geral das ordens...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/05-teoria-classicismo/",
+        "teaser": null
+      },{
+        "title": "Arte clássica",
+        "excerpt":"À medida que avançamos no recorte cronológico da Idade Moderna, a tradição clássica da arquitetura europeia se apresenta como um sistema teórico e técnico cada vez mais elaborado e como uma combinação cada vez mais diversificada de práticas construtivas e visões de mundo. As ordens de colunas dos antigos gregos...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/06-arte-classica/",
+        "teaser": null
+      },{
+        "title": "Clássico Anticlássico",
+        "excerpt":"O início do século XVI na Itália é um período de consolidação e de aquisição de desenvoltura no manejo dos elementos arquitetônicos e espaciais clássicos. Essa consolidação resulta na livre manipulação plástica de superfícies construídas e de espaços e na exploração de combinações cada vez mais elaboradas das ordens da...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/07-anticlassico/",
+        "teaser": null
+      },{
+        "title": "Barroco e classicismo",
+        "excerpt":"Da tipologia espacial ao projeto geométrico Segundo Argan, mas alertar sobre noção de “progresso” Bernini: Classicismo para além do clássico Borromini e Guarini Plautilla Bricci: círculo de Elpidio Benedetti Fachadas, plano e recessão Na Itália da segunda metade do século XVI, há uma gradual elaboração das fachadas, sobretudo nas igrejas....","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/08-barroco/",
+        "teaser": null
+      },{
+        "title": "Arquitetura acadêmica",
+        "excerpt":"Colunata do Louvre A contínua reconstrução e ampliação do Louvre é um retrato dos sistemas de poder político e cultural na França, e de como eles se transformam desde o século XII até o presente. Figura 1: Palácio do Louvre, vista aérea. Kabel (2005) Figura 2: Fases de construção dos...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/09-academicismo/",
+        "teaser": null
+      },{
+        "title": "Primeiros modernos",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/10-primeiros-modernos/",
+        "teaser": null
+      },{
+        "title": "Urbanismo como arquitetura",
+        "excerpt":"Sincretismo cultural na Etiópia Etiópia até o século XVII O império da Etiópia é o estado africano mais duradouro depois do antigo Egito, e um dos dois mais antigos reinos cristãos do mundo, junto com a Armênia (Figs. 1, 2). Figura 1: Estados Africanos até o século XVI.“The Empires of Africa...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/11-urb-como-arq/",
+        "teaser": null
+      },{
+        "title": "Barroco internacional",
+        "excerpt":"Teorias do barroco A história da arte e da arquitetura que temos acompanhado até agora é uma disciplina essencialmente europeia, tanto nos seus métodos e autores mais consagrados, quanto nos seus objetos de estudo mais comuns. Desde a consolidação da História da Arte como disciplina universitária e campo de pesquisa...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/12-barroco-intl/",
+        "teaser": null
+      },{
+        "title": "Universo em expansão",
+        "excerpt":"Nascimento da história da arte e fronteiras da arquitetura Ruínas e limites do clássico Grand Tour e Pompeia Adam em Spalato Pitoresco e arquiteturas nacionais English landscape Goethe Gótico como problema (Neo)gótico inglês? Jardim chinês O olhar ocidental tende a achatar a história da arquitetura chinesa numa espécie de “eterno...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/13-universo/",
+        "teaser": null
+      },{
+        "title": "Reação clássica",
+        "excerpt":"Classicismo para o povo Até agora, o nosso estudo da arquitetura clássica se concentrou na produção erudita dos grandes mestres e na teoria sofisticada dos tratadistas. Por mais que os textos teóricos pudessem se difundir graças à publicação impressa, o conteúdo desses tratados era, em geral, especulativo e dirigido a...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/14-reacao/",
+        "teaser": null
+      },{
+        "title": "Territórios e viação",
+        "excerpt":"Viação como estratégia espacial École des Ponts et Chaussées Em 1599, o rei da França Henrique IV cria o cargo de Grande Agente da Viação para o seu ministro Sully. No reinado de Luís XIV, essa função é incorporada à controladoria da fazenda sob a responsabilidade de Colbert. Ao contrário...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/15-territorios/",
+        "teaser": null
+      },{
+        "title": "Análise urbana",
+        "excerpt":"Ementa A tipologia edilícia e a forma urbana se influenciam mutuamente. A arquitetura responde simultaneamente às condições singulares da sua implantação no lugar, e gera condicionantes para intervenções futuras. Explore este diálogo analisando um conjunto urbanístico formado ou transformado entre os séculos XV e XVIII. Objetivo Desenvolver uma visão de...","categories": [],
+        "tags": [],
+        "url": "/tau0006/trabalho/1-analise-urbana/",
+        "teaser": null
+      },{
+        "title": "Análise de composição",
+        "excerpt":"Ementa A tipologia edilícia e a forma urbana se influenciam mutuamente. A arquitetura responde simultaneamente às condições singulares da sua implantação no lugar, e gera condicionantes para intervenções futuras. Dentro do gabarito condicionado pelo entorno ou pela tipologia da edificação, os espaços interiores se desdobram como volumes dotados de formas...","categories": [],
+        "tags": [],
+        "url": "/tau0006/trabalho/2-analise-arq/",
+        "teaser": null
+      },{
+        "title": "Permutações",
+        "excerpt":"Ementa A arquitetura clássica é um sistema de ordenamento global do ambiente construído, fornecendo ao mesmo tempo harmonia entre edificações graças a uma linguagem partilhada e uma variedade de soluções possíveis para cada problema arquitetônico. Demonstre essa afirmação permutando uma fachada clássica em outra, através de cinco passos intermediários. Objetivo...","categories": [],
+        "tags": [],
+        "url": "/tau0006/trabalho/2-permutacoes/",
+        "teaser": null
+      },{
+        "title": "Caráter",
+        "excerpt":"Ementa Converter uma composição murária em colunatas? Objetivo Refletir criticamente sobre a tradição historiográfica da arquitetura, e especialmente sobre os debates a respeito das noções de Antiguidade e Modernidade. Processo (10 pontos) Produto (10 pontos) Recursos e apoio Use o fórum da unidade e o ambiente de construção colaborativa do...","categories": [],
+        "tags": [],
+        "url": "/tau0006/trabalho/3-carater/",
+        "teaser": null
+      },{
+        "title": "Pesquisa e crítica",
+        "excerpt":"Ementa Construção de um argumento historiográfico ou teórico fundamentado em pesquisa bibliográfica e iconográfica, versando sobre um tema relacionado à arte, arquitetura, urbanismo ou paisagismo dos séculos XV a XVIII. Objetivo Refletir criticamente sobre a tradição historiográfica da arquitetura, e especialmente sobre os debates a respeito das noções de Antiguidade...","categories": [],
+        "tags": [],
+        "url": "/tau0006/trabalho/3-pesquisa/",
+        "teaser": null
+      },{
+        "title": "Pesquisa e crítica",
+        "excerpt":"Ementa Ao longo da Idade Moderna, arquitetos e teóricos abordaram o problema da legitimidade da arquitetura enquanto ofício ao mesmo tempo intelectual, técnico e artístico. Os problemas em torno da natureza da arquitetura enquanto um campo do conhecimento e das regras do ofício enquanto prática resultaram em tratados teóricos, manuais...","categories": [],
+        "tags": [],
+        "url": "/tau0006/trabalho/3-wikipedia/",
+        "teaser": null
+      }]
