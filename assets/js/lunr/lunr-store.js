35bf0a199f671ebac21983c273d03a852a006173
Automated build <runner@github.com>
2021-11-20 17:58:27 +0000

    Revisão geral da ordem dos conteúdos

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,98 +1,146 @@
 var store = [{
-        "title": "Apresentação e acolhimento",
-        "excerpt":"Boas-vindas Esta é a Turma C da disciplina História da Arquitetura e da Arte II. Vamos acompanhar vocês num percurso que vai do século XV ao XVIII. Começamos falando um pouco sobre como a disciplina vai ser desenvolvida este semestre — se você está com dúvidas sobre o funcionamento das...","categories": [],
-        "tags": [],
-        "url": "/tau0006/aula/00-intro/",
-        "teaser": null
-      },{
-        "title": "Arquitetura do humanismo",
-        "excerpt":"Introdução à arquitetura da Idade Moderna O nosso percurso começa em 1420, mais ou menos. Meio século para mais ou para menos, mas isso não importa. Exatamente em 1420, termina a reconstrução do palácio imperial em Pequim, conhecido no Ocidente como a “Cidade proibida” (fig. 1). No mesmo ano, começa a...","categories": [],
-        "tags": [],
-        "url": "/tau0006/aula/01-humanismo/",
-        "teaser": null
-      },{
         "title": "Rotas comerciais e paradigmas urbanísticos",
-        "excerpt":"First terrestrial globe - Martin Behaim 1492 by Drones Imaging on Sketchfab Parcelamento do solo e paisagem urbana Os elementos e objetos da arquitetura têm uma ordem de precedência específica. A tipologia edilícia determina os padrões de ocupação do espaço urbano e rural: isso porque os tipos de casas usados...","categories": [],
+        "excerpt":"Mundos conectados no século XIV A “Globalização” é tão antiga quanto o comércio. A mudança nas redes comerciais e nas relações de poder em escala mundial, com a expansão europeia dos séculos XVI a XIX, foi tão brutal que às vezes perdemos de vista o quanto essas redes e relações...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/02-rotas/",
+        "url": "/tau0006/aula/rotas/",
         "teaser": null
       },{
-        "title": "Renascimento e historiografia",
-        "excerpt":"Nesta sequência, vamos abordar a arquitetura do século XV na Europa e na Ásia central sob os pontos de vista da construção e da espacialidade. O desenvolvimento de sistemas e processos construtivos e a transformação nas configurações e relações espaciais são duas das principais questões que pertencem ao domínio da...","categories": [],
-        "tags": [],
-        "url": "/tau0006/aula/03-renascimento/",
-        "teaser": null
-      },{
-        "title": "Cidades ideais e cidades possíveis",
+        "title": "Cidades ideais",
         "excerpt":"Neste tópico nós vamos tratar do pensamento e da prática de produção das cidades na Europa e na Ásia dos séculos XV e XVI. A primeira parte aborda o que se convencionou chamar de cidades ideais na teoria da arquitetura na Itália e na China. A segunda parte observa a...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/04-cidades-ideais/",
+        "url": "/tau0006/aula/cidades-ideais/",
+        "teaser": null
+      },{
+        "title": "Cidades possíveis",
+        "excerpt":"História da arquitetura como história do espaço O domínio do espaço é um dos temas estruturantes na nossa abordagem da arquitetura do Renascimento. Desde o início da construção da cúpula da catedral de Florença, em 1420, até as superfícies tortuosas do barroco tardio e do rococó, no século XVIII, a...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/cidades-possiveis/",
+        "teaser": null
+      },{
+        "title": "Sistemas espaciais e abóbadas",
+        "excerpt":"Nesta sequência, vamos abordar a arquitetura do século XV na Europa e na Ásia central sob os pontos de vista da construção e da espacialidade. O desenvolvimento de sistemas e processos construtivos e a transformação nas configurações e relações espaciais são duas das principais questões que pertencem ao domínio da...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/sistemas-espaciais/",
+        "teaser": null
+      },{
+        "title": "Linguagem clássica",
+        "excerpt":"Invenção do classicismo Na seção anterior, vimos que a arquitetura do Renascimento é humanista na medida em que ela reconhece e articula uma reflexão sobre a complexidade da experiência humana. Essa complexidade se manifesta na aparente contradição entre, por um lado, o ser humano feito “à imagem e semelhança de...","categories": [],
+        "tags": [],
+        "url": "/tau0006/aula/linguagem/",
         "teaser": null
       },{
         "title": "Teoria do classicismo",
-        "excerpt":"Teoria das ordens Ordens por divisões tripartites sucessivas (Vitrúvio e Alberti). Demonstrar com base e entablamento. Ordens com proporções numéricas (Vinhola e Palladio) Teoria das molduras Na tradição arquitetônica europeia, as ordens clássicas são o instrumento por excelência para realizar articulações de superfícies e espaços. A aparência geral das ordens...","categories": [],
-        "tags": [],
-        "url": "/tau0006/aula/05-teoria-classicismo/",
-        "teaser": null
-      },{
-        "title": "Arte clássica",
         "excerpt":"À medida que avançamos no recorte cronológico da Idade Moderna, a tradição clássica da arquitetura europeia se apresenta como um sistema teórico e técnico cada vez mais elaborado e como uma combinação cada vez mais diversificada de práticas construtivas e visões de mundo. As ordens de colunas dos antigos gregos...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/06-arte-classica/",
+        "url": "/tau0006/aula/teoria/",
         "teaser": null
       },{
         "title": "Clássico Anticlássico",
         "excerpt":"O início do século XVI na Itália é um período de consolidação e de aquisição de desenvoltura no manejo dos elementos arquitetônicos e espaciais clássicos. Essa consolidação resulta na livre manipulação plástica de superfícies construídas e de espaços e na exploração de combinações cada vez mais elaboradas das ordens da...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/07-anticlassico/",
+        "url": "/tau0006/aula/anticlassico/",
         "teaser": null
       },{
-        "title": "Barroco e classicismo",
-        "excerpt":"Da tipologia espacial ao projeto geométrico Segundo Argan, mas alertar sobre noção de “progresso” Bernini: Classicismo para além do clássico Borromini e Guarini Plautilla Bricci: círculo de Elpidio Benedetti Fachadas, plano e recessão Na Itália da segunda metade do século XVI, há uma gradual elaboração das fachadas, sobretudo nas igrejas....","categories": [],
+        "title": "Cidade clássica",
+        "excerpt":"Geometrias da fortificação à praça real Geometria e fortificação Castelos japoneses Controle do território Bastides construídas num contexto de organização do território e centralização administrativa do poder real. Figura 1: Aigas-Mòrtas, França, 1248.Circhirello:2011vue? Figura 2: Monpazier, possessões inglesas na França, 1285.Mairiemonpazier:2013vue? Ensaios ideais Figura 3: Filarete, cidade ideal de Sforzinda,...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/08-barroco/",
+        "url": "/tau0006/aula/cidade-classica/",
         "teaser": null
       },{
         "title": "Arquitetura acadêmica",
         "excerpt":"Colunata do Louvre A contínua reconstrução e ampliação do Louvre é um retrato dos sistemas de poder político e cultural na França, e de como eles se transformam desde o século XII até o presente. Figura 1: Palácio do Louvre, vista aérea. Kabel (2005) Figura 2: Fases de construção dos...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/09-academicismo/",
-        "teaser": null
-      },{
-        "title": "Primeiros modernos",
-        "excerpt":" Bibliografia   ","categories": [],
-        "tags": [],
-        "url": "/tau0006/aula/10-primeiros-modernos/",
+        "url": "/tau0006/aula/academicismo/",
         "teaser": null
       },{
         "title": "Urbanismo como arquitetura",
-        "excerpt":"Sincretismo cultural na Etiópia Etiópia até o século XVII O império da Etiópia é o estado africano mais duradouro depois do antigo Egito, e um dos dois mais antigos reinos cristãos do mundo, junto com a Armênia (Figs. 1, 2). Figura 1: Estados Africanos até o século XVI.“The Empires of Africa...","categories": [],
+        "excerpt":"Sincretismo cultural na Etiópia Etiópia até o século XVII O império da Etiópia é o estado africano mais duradouro depois do antigo Egito, e um dos dois mais antigos reinos cristãos do mundo, junto com a Armênia (Figs. 1, 2). Figura 1: Estados Africanos até o século XVI. The Map Archive“The...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/11-urb-como-arq/",
+        "url": "/tau0006/aula/urb-como-arq/",
         "teaser": null
       },{
         "title": "Barroco internacional",
         "excerpt":"Teorias do barroco A história da arte e da arquitetura que temos acompanhado até agora é uma disciplina essencialmente europeia, tanto nos seus métodos e autores mais consagrados, quanto nos seus objetos de estudo mais comuns. Desde a consolidação da História da Arte como disciplina universitária e campo de pesquisa...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/12-barroco-intl/",
+        "url": "/tau0006/aula/barroco-intl/",
         "teaser": null
       },{
         "title": "Universo em expansão",
-        "excerpt":"Nascimento da história da arte e fronteiras da arquitetura Ruínas e limites do clássico Grand Tour e Pompeia Adam em Spalato Pitoresco e arquiteturas nacionais English landscape Goethe Gótico como problema (Neo)gótico inglês? Jardim chinês O olhar ocidental tende a achatar a história da arquitetura chinesa numa espécie de “eterno...","categories": [],
+        "excerpt":" Antiquarismo e história da arte   Nascimento da história da arte e fronteiras da arquitetura    Ruínas e limites do clássico   Grand Tour e Pompeia Adam em Spalato    Pitoresco e arquiteturas nacionais   English landscape Goethe    Gótico como problema  (Neo)gótico inglês?     Bibliografia   ","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/13-universo/",
+        "url": "/tau0006/aula/universo/",
         "teaser": null
       },{
         "title": "Reação clássica",
         "excerpt":"Classicismo para o povo Até agora, o nosso estudo da arquitetura clássica se concentrou na produção erudita dos grandes mestres e na teoria sofisticada dos tratadistas. Por mais que os textos teóricos pudessem se difundir graças à publicação impressa, o conteúdo desses tratados era, em geral, especulativo e dirigido a...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/14-reacao/",
+        "url": "/tau0006/aula/reacao/",
         "teaser": null
       },{
         "title": "Territórios e viação",
         "excerpt":"Viação como estratégia espacial École des Ponts et Chaussées Em 1599, o rei da França Henrique IV cria o cargo de Grande Agente da Viação para o seu ministro Sully. No reinado de Luís XIV, essa função é incorporada à controladoria da fazenda sob a responsabilidade de Colbert. Ao contrário...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/15-territorios/",
+        "url": "/tau0006/aula/territorios/",
+        "teaser": null
+      },{
+        "title": "Roteiro para análise urbana",
+        "excerpt":"Roteiro para análise urbana Objetivo: Criar um inventário por meio do reconhecimento e levantamento de dados e das condições existentes no sítio, de modo a ter um maior domínio da própria intervenção e de sua inserção no ambiente urbano existente. Ambiente natural Condições de insolação Orientação do norte magnético e...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/analise-urbana/",
+        "teaser": null
+      },{
+        "title": "Como vamos trabalhar",
+        "excerpt":"Nossas ferramentas Moodle Neste semestre, você vai continuar estudando em modo remoto. Todo o conteúdo e as atividades da disciplina estão no ambiente virtual de aprendizagem Aprender 3. Esse ambiente usa um sistema livre de ensino a distância chamado Moodle (pronuncia-se “múdou”). Por isso, na maioria das vezes vamos nos referir...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/metodologia/",
+        "teaser": null
+      },{
+        "title": "Leitura e estudo",
+        "excerpt":"Como estudar Como estamos trabalhando a distância, a leitura e o estudo individual são componentes centrais da nossa disciplina. Não espere que a videoconferência vá suprir todo o conteúdo do qual você precisa para adquirir as competências e realizar as atividades! Antes, a videoconferência é uma ocasião para esclarecer dúvidas...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/bibliografia/",
+        "teaser": null
+      },{
+        "title": "TAU 0006 · História da Arquitetura e da Arte II",
+        "excerpt":"Conteúdo Leituras e Atividades Prazo UNIDADE I · REDES URBANAS NA FUNDAÇÃO DO MUNDO MODERNO Trabalho: Análise urbana 01/02 a 26/02 1 Arquitetura do humanismo – Introdução à arquitetura da Idade Moderna – Arquitetura clássica entre a prática e a teoria – Problemas de historiografia e crítica – Construção medieval...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/cronograma/",
+        "teaser": null
+      },{
+        "title": "Perguntas frequentes",
+        "excerpt":"Como é feito o controle de frequência? A UnB considera oficialmente esta disciplina como sendo “presencial”, portanto temos que controlar a “frequência” ao longo do semestre. Assim, você deve se fazer “presente” em pelo menos 75 % da carga horária do semestre (45 de 60), participando ativa e produtivamente das discussões...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/faq/",
+        "teaser": null
+      },{
+        "title": "Apresentação e acolhimento",
+        "excerpt":"Boas-vindas Esta é a Turma C da disciplina História da Arquitetura e da Arte II. Vamos acompanhar vocês num percurso que vai do século XV ao XVIII. Começamos falando um pouco sobre como a disciplina vai ser desenvolvida este semestre — se você está com dúvidas sobre o funcionamento das...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/intro/",
+        "teaser": null
+      },{
+        "title": "Páginas do livro-texto",
+        "excerpt":"Era páginas Tópico Un. Mód. Aula v–vii 3 Prefácio 0 0 0 1200 425 1 Yingzao Fashi I iii 5 436 1 Grande Zimbábue I i 2 1400 455–459 5 Introdução 0 0 0 460–468 9 Américas II vi 12 469–473 5 Veneza – Império Otomano I i 2 474–490...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/livro-texto/",
+        "teaser": null
+      },{
+        "title": "Objetivos de aprendizagem",
+        "excerpt":"O objetivo geral da disciplina é adquirir um domínio operativo da arquitetura e do urbanismo da Idade Moderna (séculos XV a XVIII) enquanto objeto de pesquisa historiográfica e enquanto tradição fundadora do campo profissional, estético e técnico da contemporaneidade. Os quatro objetivos específicos correspondem, respectivamente, à visão de conjunto da...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/objetivos/",
+        "teaser": null
+      },{
+        "title": "TAU 0006 · História da Arquitetura e da Arte II",
+        "excerpt":"Apresentação Esta disciplina dá sequência ao conteúdo de história da arquitetura em ordem cronológica e visando a atingir uma perspectiva mundial. O fundamento da cadeia de disciplinas de Teoria e História da Arquitetura e do Urbanismo, incluindo Estética e História das Artes, é subsidiar, por meio do conhecimento de repertórios...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/plano/",
+        "teaser": null
+      },{
+        "title": "Sobre a disciplina",
+        "excerpt":"TAU 0006 dá sequência ao conteúdo de história da arquitetura em ordem cronológica e visando a atingir uma perspectiva mundial. O fundamento da cadeia de disciplinas de Teoria e História da Arquitetura e do Urbanismo, incluindo Estética e História das Artes, é subsidiar, por meio do conhecimento de repertórios formais e...","categories": [],
+        "tags": [],
+        "url": "/tau0006/plano/sobre-disciplina/",
         "teaser": null
       },{
         "title": "Análise urbana",
@@ -129,5 +177,11 @@ var store = [{
         "excerpt":"Ementa Ao longo da Idade Moderna, arquitetos e teóricos abordaram o problema da legitimidade da arquitetura enquanto ofício ao mesmo tempo intelectual, técnico e artístico. Os problemas em torno da natureza da arquitetura enquanto um campo do conhecimento e das regras do ofício enquanto prática resultaram em tratados teóricos, manuais...","categories": [],
         "tags": [],
         "url": "/tau0006/trabalho/3-wikipedia/",
+        "teaser": null
+      },{
+        "title": "Resenha e discussão de texto",
+        "excerpt":"Ementa Reflexão crítica sobre a leitura da semana, preparatória para a discussão em aula na quinta-feira. Produto (3 pontos) Em um ou dois parágrafos (aproximadamente 3 a 5 frases por parágrafo) e usando as suas próprias palavras, discuta os principais argumentos levantados pela leitura indicada para esta semana. Não é...","categories": [],
+        "tags": [],
+        "url": "/tau0006/trabalho/resenha/",
         "teaser": null
       }]
