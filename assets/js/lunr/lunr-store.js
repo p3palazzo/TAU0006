5131566e3fca99c977d44adca0956a372f30657e
Automated build <runner@github.com>
2022-10-14 13:58:55 +0000

    Corrige maiúsculas do repo/baseurl

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,174 +2,174 @@ var store = [{
         "title": "Rotas comerciais e paradigmas urbanísticos",
         "excerpt":"Mundos conectados no século XIV Figura 1: Martin Behaim, globo terrestre, 1492. Digitalizado por Drones Imaging. A “Globalização” é tão antiga quanto o comércio. A mudança nas redes comerciais e nas relações de poder em escala mundial, com a expansão europeia dos séculos XVI a XIX, foi tão brutal que...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/rotas/",
+        "url": "/TAU0006/aula/rotas/",
         "teaser": null
       },{
         "title": "Cidades ideais",
         "excerpt":"Neste tópico nós vamos tratar do pensamento e da prática de produção das cidades na Europa e na Ásia dos séculos XV e XVI. A primeira parte aborda o que se convencionou chamar de cidades ideais na teoria da arquitetura na Itália e na China. A segunda parte observa a...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/cidades-ideais/",
+        "url": "/TAU0006/aula/cidades-ideais/",
         "teaser": null
       },{
         "title": "Cidades possíveis",
         "excerpt":"Espacialidade e vazio na pintura chinesa O domínio do espaço é um dos temas estruturantes na nossa abordagem da arquitetura do Renascimento. Desde o início da construção da cúpula da catedral de Florença, em 1420, até as superfícies tortuosas do barroco tardio e do rococó, no século XVIII, a manipulação...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/cidades-possiveis/",
+        "url": "/TAU0006/aula/cidades-possiveis/",
         "teaser": null
       },{
         "title": "Sistemas espaciais e abóbadas",
         "excerpt":"Teoria da arquitetura na Idade Moderna Nesta sequência, vamos abordar a arquitetura do século XV na Europa e na Ásia central sob os pontos de vista da construção e da espacialidade. O desenvolvimento de sistemas e processos construtivos e a transformação nas configurações e relações espaciais são duas das principais...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/sistemas-espaciais/",
+        "url": "/TAU0006/aula/sistemas-espaciais/",
         "teaser": null
       },{
         "title": "Linguagem clássica",
         "excerpt":"Invenção do classicismo Na seção anterior, vimos que a arquitetura do Renascimento é humanista na medida em que ela reconhece e articula uma reflexão sobre a complexidade da experiência humana. Essa complexidade se manifesta na aparente contradição entre, por um lado, o ser humano feito “à imagem e semelhança de...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/linguagem/",
+        "url": "/TAU0006/aula/linguagem/",
         "teaser": null
       },{
         "title": "Arquiteturas humanistas",
         "excerpt":"Arquitetura do humanismo À medida que avançamos no recorte cronológico da Idade Moderna, a tradição clássica da arquitetura europeia se apresenta como um sistema teórico e técnico cada vez mais elaborado e como uma combinação cada vez mais diversificada de práticas construtivas e visões de mundo. As ordens de colunas...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/teoria/",
+        "url": "/TAU0006/aula/teoria/",
         "teaser": null
       },{
         "title": "Clássico Anticlássico",
         "excerpt":"Composição O início do século XVI na Itália é um período de consolidação e de aquisição de desenvoltura no manejo dos elementos arquitetônicos e espaciais clássicos. Essa consolidação resulta na livre manipulação plástica de superfícies construídas e de espaços e na exploração de combinações cada vez mais elaboradas das ordens...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/anticlassico/",
+        "url": "/TAU0006/aula/anticlassico/",
         "teaser": null
       },{
         "title": "Cidade clássica",
         "excerpt":"Geometrias da fortificação Controle do território Figura 1: Planta de 1806 mostrando o castelo de Himeji e áreas residenciais dos samurais. Acervo do museu histórico da província de Hyōgo Bastides são cidades novas construídas num contexto de organização do território e centralização administrativa do poder real, nos últimos séculos da...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/cidade-classica/",
+        "url": "/TAU0006/aula/cidade-classica/",
         "teaser": null
       },{
         "title": "Arquitetura acadêmica",
         "excerpt":"Colunata do Louvre A contínua reconstrução e ampliação do Louvre é um retrato dos sistemas de poder político e cultural na França, e de como eles se transformam desde o século XII até o presente. Figura 1: Palácio do Louvre, vista aérea. Kabel (2005) Figura 2: Fases de construção dos...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/academicismo/",
+        "url": "/TAU0006/aula/academicismo/",
         "teaser": null
       },{
         "title": "Urbanismo como arquitetura",
         "excerpt":"Sincretismo cultural na Etiópia Etiópia até o século XVII O império da Etiópia é o estado africano mais duradouro depois do antigo Egito, e um dos dois mais antigos reinos cristãos do mundo, junto com a Armênia (Figs. 1, 2). Figura 1: Estados Africanos até o século XVI. Fonte: The Map...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/urb-como-arq/",
+        "url": "/TAU0006/aula/urb-como-arq/",
         "teaser": null
       },{
         "title": "Barroco internacional",
         "excerpt":"Teorias do barroco A história da arte e da arquitetura que temos acompanhado até agora é uma disciplina essencialmente europeia, tanto nos seus métodos e autores mais consagrados, quanto nos seus objetos de estudo mais comuns. Desde a consolidação da História da Arte como disciplina universitária e campo de pesquisa...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/barroco-intl/",
+        "url": "/TAU0006/aula/barroco-intl/",
         "teaser": null
       },{
         "title": "Universo em expansão",
         "excerpt":"Sociedade burguesa Mercado de arte Privacidade doméstica burguesa e a sua influência na aristocracia Rococó Antiquarismo e história da arte Grand Tour Figura 1: Claude Gellée, dito Lorrain, Paisagem com o templo da Sibila de Tivoli, 1630–35 Figura 2: Claude Lorrain, Vista idealizada de Tivoli, 1644 Figura 3: Nicolaes Berchem,...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/universo/",
+        "url": "/TAU0006/aula/universo/",
         "teaser": null
       },{
         "title": "Reação clássica",
         "excerpt":"Classicismo para o povo Até agora, o nosso estudo da arquitetura clássica se concentrou na produção erudita dos grandes mestres e na teoria sofisticada dos tratadistas. Por mais que os textos teóricos pudessem se difundir graças à publicação impressa, o conteúdo desses tratados era, em geral, especulativo e dirigido a...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/reacao/",
+        "url": "/TAU0006/aula/reacao/",
         "teaser": null
       },{
         "title": "Territórios e viação",
         "excerpt":"Viação como estratégia espacial École des Ponts et Chaussées Em 1599, o rei da França Henrique IV cria o cargo de Grande Agente da Viação para o seu ministro Sully. No reinado de Luís XIV, essa função é incorporada à controladoria da fazenda sob a responsabilidade de Colbert. Ao contrário...","categories": [],
         "tags": [],
-        "url": "/tau0006/aula/territorios/",
+        "url": "/TAU0006/aula/territorios/",
         "teaser": null
       },{
         "title": "Roteiro para análise urbana",
         "excerpt":"Roteiro para análise urbana Objetivo: Criar um inventário por meio do reconhecimento e levantamento de dados e das condições existentes no sítio, de modo a ter um maior domínio da própria intervenção e de sua inserção no ambiente urbano existente. Ambiente natural Condições de insolação Orientação do norte magnético e...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/analise-urbana/",
+        "url": "/TAU0006/plano/analise-urbana/",
         "teaser": null
       },{
         "title": "Apresentação e acolhimento",
         "excerpt":"Boas-vindas Esta é a Turma C da disciplina História da Arquitetura e da Arte II. Vamos acompanhar vocês num percurso que vai do século XV ao XVIII. Começamos falando um pouco sobre como a disciplina vai ser desenvolvida este semestre — se você está com dúvidas sobre o funcionamento das...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/intro/",
+        "url": "/TAU0006/plano/intro/",
         "teaser": null
       },{
         "title": "Sobre a disciplina",
         "excerpt":"TAU 0006 dá sequência ao conteúdo de história da arquitetura em ordem cronológica e visando a atingir uma perspectiva mundial. O fundamento da cadeia de disciplinas de Teoria e História da Arquitetura e do Urbanismo, incluindo Estética e História das Artes, é subsidiar, por meio do conhecimento de repertórios formais e...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/sobre-disciplina/",
+        "url": "/TAU0006/plano/sobre-disciplina/",
         "teaser": null
       },{
         "title": "Avaliação",
         "excerpt":"Critérios para aprovação na disciplina O que esperamos de todas as entregas é atenção ao que foi pedido, pesquisa e leitura criteriosa do material necessário à realização do trabalho, reflexão crítica, clareza e capricho na execução. Para obter aprovação na disciplina, você precisa atender a todos os quatro requisitos seguintes:...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/avalia/",
+        "url": "/TAU0006/plano/avalia/",
         "teaser": null
       },{
         "title": "Leitura e estudo",
         "excerpt":"A leitura e o estudo individual são componentes centrais da nossa disciplina. Não espere que a aula expositiva vá suprir todo o conteúdo do qual você precisa para adquirir as competências e realizar os trabalhos! Antes, o encontro em aula é uma ocasião para esclarecer dúvidas sobre pontos do conteúdo...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/bibliografia/",
+        "url": "/TAU0006/plano/bibliografia/",
         "teaser": null
       },{
         "title": "Perguntas frequentes",
         "excerpt":"Como é feito o controle de frequência? A UnB considera oficialmente esta disciplina como sendo “presencial”, portanto temos que controlar a “frequência” ao longo do semestre. Assim, você deve se fazer “presente” em pelo menos 75 % da carga horária do semestre (45 de 60), participando ativa e produtivamente das discussões...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/faq/",
+        "url": "/TAU0006/plano/faq/",
         "teaser": null
       },{
         "title": "Metodologia e recursos de apoio",
         "excerpt":"Moodle Todo o conteúdo e as ementas dos trabalhos da disciplina estão no ambiente virtual de aprendizagem Aprender 3. Esse ambiente usa um sistema livre de ensino a distância chamado Moodle (pronuncia-se “múdou”). Por isso, na maioria das vezes vamos nos referir à plataforma virtual como o Moodle. O login do...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/metodologia/",
+        "url": "/TAU0006/plano/metodologia/",
         "teaser": null
       },{
         "title": "Objetivos de aprendizagem",
         "excerpt":"Objetivo geral O objetivo geral da disciplina é adquirir um domínio operativo da arquitetura e do urbanismo da Idade Moderna (séculos XV a XVIII) enquanto objeto de pesquisa historiográfica e enquanto tradição fundadora do campo profissional, estético e técnico da contemporaneidade. Os quatro objetivos específicos correspondem, respectivamente, à visão de...","categories": [],
         "tags": [],
-        "url": "/tau0006/plano/objetivos/",
+        "url": "/TAU0006/plano/objetivos/",
         "teaser": null
       },{
         "title": "Análise urbana",
         "excerpt":"Os instrumentos de análise têm de ser suficientemente genéricos para que possam ser aplicados a qualquer cidade; em suma, eles servem para começar a observar as cidades e nelas reconhecer algumas lógicas recorrentes. Com efeito, um dos aspectos fascinantes do estudo das cidades é que, apesar de suas diferenças e...","categories": [],
         "tags": [],
-        "url": "/tau0006/trabalho/analise-urbana/",
+        "url": "/TAU0006/trabalho/analise-urbana/",
         "teaser": null
       },{
         "title": "Análise arquitetônica",
         "excerpt":"Objetivo Dominar e ser capaz de empregar os elementos do vocabulário e a gramática das tradições arquitetônicas da era moderna, com ênfase na linguagem clássica. Ementa Premissa Figura 1: Bernardo Vittone, projeto de uma capela, em Istruzioni diverse, 1766 Figura 2: Claude Perrault, escadaria dos Embaixadores, Louvre, c. 1672 (destruída) A...","categories": [],
         "tags": [],
-        "url": "/tau0006/trabalho/analise-arq/",
+        "url": "/TAU0006/trabalho/analise-arq/",
         "teaser": null
       },{
         "title": "Pesquisa e crítica",
         "excerpt":"Objetivo de aprendizagem Saber pesquisar Refletir criticamente sobre a tradição historiográfica da arquitetura, e especialmente sobre os debates a respeito das noções de Antiguidade e Modernidade. Ementa Ao longo da Idade Moderna, arquitetos e teóricos abordaram o problema da legitimidade da arquitetura enquanto ofício ao mesmo tempo intelectual, técnico e...","categories": [],
         "tags": [],
-        "url": "/tau0006/trabalho/wikipedia/",
+        "url": "/TAU0006/trabalho/wikipedia/",
         "teaser": null
       },{
         "title": "Instrumentos e materiais",
         "excerpt":"Para fazer as atividades da disciplina, você vai precisar de um conjunto básico de instrumentos de desenho arquitetônico e modelagem, seja à mão ou digital. Desenho à mão Materiais indicados Uma superfície lisa e macia para desenhar: prancheta portátil ou de pé, mesa de jantar ou tábua em MDF com...","categories": [],
         "tags": [],
-        "url": "/tau0006/trabalho/materiais/",
+        "url": "/TAU0006/trabalho/materiais/",
         "teaser": null
       },{
         "title": "Análise arquitetônica",
         "excerpt":"Objetivo de aprendizagem Saber projetar Dominar e ser capaz de empregar os elementos do vocabulário e a gramática das tradições arquitetônicas da era moderna, com ênfase na linguagem clássica. Processo Caderno de croquis e anotações Ao longo do semestre, você deve constituir um caderno com croquis e anotações referentes ao...","categories": [],
         "tags": [],
-        "url": "/tau0006/trabalho/prova/",
+        "url": "/TAU0006/trabalho/prova/",
         "teaser": null
       },{
         "title": "Pesquisa e análise",
         "excerpt":"[Após o terremoto de Lisboa em 1755,] Portugal não ficou estagnado. O centro de Lisboa foi reconstruído pelos engenheiros militares Manuel da Maia, Eugénio dos Santos e Carlos Mardel, entre outros, cujas ideias acerca do planeamento urbano eram consoantes com os projetos do [marquês de] Pombal para Lisboa: uma cidade...","categories": [],
         "tags": [],
-        "url": "/tau0006/trabalho/pesquisa-analise/",
+        "url": "/TAU0006/trabalho/pesquisa-analise/",
         "teaser": null
       },{
         "title": "Resenha e discussão de texto",
         "excerpt":"Objetivo de aprendizagem Conhecer os fatos urbanos Estudar as obras e os processos descritos na bibliografia, refletindo criticamente sobre inclusões, exclusões e abordagens nas escolhas operadas pelos autores. Produto Resumo da leitura (1 ponto) Em um parágrafo (aproximadamente 3 a 5 frases por parágrafo) e usando as suas próprias palavras,...","categories": [],
         "tags": [],
-        "url": "/tau0006/trabalho/resenha/",
+        "url": "/TAU0006/trabalho/resenha/",
         "teaser": "https://hcommons.org/app/uploads/sites/1002372/2022/05/teaser-640px-crespi-scaffale.jpg"
       }]
